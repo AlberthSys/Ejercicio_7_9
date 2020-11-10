6f6cf539f40dd52b0eb40bb8c5f6688efea3d04d
Alberth <bcajavier@gmail.com>
2020-11-10 20:29:52 +0100

    More Clear
    Better def var

--- average.cs
+++ average.cs
@@ -7,7 +7,7 @@ public class Average
 {
 	public static void Main()
 	{
-		double fisrtnumber = 0, anotherNumber, counting = 0, totalAverage;
+		double fisrtnumber = 0, b, countingNumbersInput = 0, totalAverage;
 		string input_console;
 
 		Console.Write("Insert a mark: ");
@@ -16,32 +16,30 @@ public class Average
         try
         {
 
-		while (input_console != "end")
-		{
-			anotherNumber = Convert.ToDouble(input_console);
+			while (input_console != "end")
+			{  
+			b = Convert.ToDouble(input_console);
 
-			counting++;   //counter of numbers
+				countingNumbersInput++;   //counter of numbers
 
-			fisrtnumber += anotherNumber;  // sum of all the notes
+			fisrtnumber += b;  // sum of all the notes
 
 			Console.Write("Insert a mark: ");
 			input_console = Console.ReadLine();
-		}
+			}
 
-		if (counting != 0)
-		{
-			totalAverage = fisrtnumber / counting;
+			if (countingNumbersInput != 0)
+			{
+			totalAverage = fisrtnumber / countingNumbersInput;
 			Console.WriteLine("The average is {0}.", totalAverage);
-		}
-
-		else if (input_console=="end")
-		{
+			}
+			else if (input_console=="end")
+			{
 			Console.WriteLine("Bye!");
-		}
-
-	}catch (Exception e)
-		{
+			}
+			}catch (Exception e)
+			{
 			Console.WriteLine("Need one number please");
-		}
+			}
 	}
 }
