4cc9781b866984a92ccced40030d9f3ddf84cd98
Alberth <73870637+AlberthSys@users.noreply.github.com>
2020-11-10 20:31:49 +0100

    Update average.cs

--- average.cs
+++ average.cs
@@ -15,31 +15,30 @@ public class Average
 
         try
         {
-
-			while (input_console != "end")
-			{  
+		while (input_console != "end")
+		{  
 			b = Convert.ToDouble(input_console);
 
-				countingNumbersInput++;   //counter of numbers
+			countingNumbersInput++;   //counter of numbers
 
 			fisrtnumber += b;  // sum of all the notes
 
 			Console.Write("Insert a mark: ");
 			input_console = Console.ReadLine();
-			}
+		}
 
-			if (countingNumbersInput != 0)
-			{
+		if (countingNumbersInput != 0)
+		{
 			totalAverage = fisrtnumber / countingNumbersInput;
 			Console.WriteLine("The average is {0}.", totalAverage);
-			}
-			else if (input_console=="end")
-			{
+		}
+		else if (input_console=="end")
+		{
 			Console.WriteLine("Bye!");
-			}
-			}catch (Exception e)
-			{
-			Console.WriteLine("Need one number please");
-			}
+		}
+	}catch (Exception e)
+	{
+		Console.WriteLine("Need one number please");
+	}
 	}
 }
