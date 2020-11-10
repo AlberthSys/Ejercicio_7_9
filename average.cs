1cf2f0314681d165bf1c0bd40cbd23090757fd10
Alberth <bcajavier@gmail.com>
2020-11-10 20:23:17 +0100

    Clear and working
    Idea for average!

--- average.cs
+++ average.cs
@@ -1,20 +1,47 @@
+﻿
 using System;
-public class Average{
-public static void Main(){
-double a=0, b, c=0, d;
-string s;
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();	// we read the mark as string to check the word "end"	 
-while(s!="end"){  // if the mark isn't an end then we pass it to double to work with it
-b=Convert.ToDouble(s);
-c++;   //counter of numbers
-a+=b;  // sum of all the notes
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();
-}
-if(c!=0){
-	d=a/c;  //calculate the average
-Console.WriteLine("The average is {0}.",d);
-}
-}
+
+//CALCULATE THE AVAREGE
+
+public class Average
+{
+	public static void Main()
+	{
+		double fisrtnumber = 0, anotherNumber, counting = 0, totalAverage;
+		string input_console;
+
+		Console.Write("Insert a mark: ");
+		input_console = Console.ReadLine(); // Check Condition
+
+        try
+        {
+
+			while (input_console != "end")
+			{
+				anotherNumber = Convert.ToDouble(input_console);
+
+				counting++;   //counter of numbers
+
+				fisrtnumber += anotherNumber;  // sum of all the notes
+
+				Console.Write("Insert a mark: ");
+				input_console = Console.ReadLine();
+			}
+
+			if (counting != 0)
+			{
+				totalAverage = fisrtnumber / counting;
+				Console.WriteLine("The average is {0}.", totalAverage);
+			}
+
+			else if (input_console=="end")
+			{
+				Console.WriteLine("Bye!");
+			}
+
+		}catch (Exception e)
+		{
+			Console.WriteLine("Need one numeber please");
+		}
+	}
 }
